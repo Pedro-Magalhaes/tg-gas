f35555b564d99a95498b8ef1b8900ef3d193fcb0
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2025-01-04 22:00:27 -0300

    melhora código de geração de distancias

--- geradora_min_dist.py
+++ geradora_min_dist.py
@@ -1,23 +1,31 @@
+import re
 import pandas as pd
 
 from graph import GasGraph, Kruskal
 
-base_tratada = "./base_tratada_tgs.xlsx"
-# base_tratada = "./atualizado.xlsx"
+# base_tratada = "./base_tratada_tgs.xlsx"
+base_tratada = "./df_TGN.xlsx"
+# base_tratada = "./atualizado.xlsx"  #df2_TGN
 
-gas_points_raw = pd.read_excel(open(base_tratada, "rb"), decimal=',')
-gas_points = gas_points_raw.rename(str.lower,axis='columns')
-gas_points = gas_points.rename(columns={"latitude": "lat", "longitude": "lon"})
+volumeProperty = 'demand22a24'
+typeProperty = 'type'
 
-gas_points['name'] = gas_points['name'].str.strip()
-gas_points['id'] = gas_points['id'].str.strip()
-gas_points['lat'] = gas_points['lat'].astype(float)
-gas_points['lon'] = gas_points['lon'].astype(float)
-gas_points.dropna()
-gas_points = gas_points.sort_values(by=['id'], ascending=True)
-
-gas_points = gas_points.drop_duplicates(['id'])
-
+df = pd.read_excel(open(base_tratada, "rb"), decimal=',')  # Leitura do arquivo
+gas_points = (
+    df.rename(str.lower, axis='columns')  # Renomear colunas para minúsculas
+    .rename(columns={"latitude": "lat", "longitude": "lon"})  # Renomear colunas específicas
+    .assign(  # Aplicar transformações nas colunas
+        name=lambda df: df['name'].str.strip(),
+        id=lambda df: df['id'].str.strip(),
+        lat=lambda df: df['lat'].astype(float),
+        lon=lambda df: df['lon'].astype(float),
+        volumeProperty= lambda df: df[volumeProperty].astype(float)
+    )
+    .loc[:, ['id', 'name', 'lat', 'lon', typeProperty, volumeProperty]]  # Selecionar colunas
+    .dropna()  # Remover linhas com valores ausentes
+    .drop_duplicates(['id'])  # Remover duplicatas com base em 'id'
+    .sort_values(by=['id'], ascending=True)  # Ordenar pelos 'id'
+)
 
 mst = Kruskal(gas_points)
 listaVizinhos = []
@@ -29,8 +37,21 @@ vizinhos = pd.DataFrame(listaVizinhos, columns=['De', 'Para', 'Custo'])
 
 grafo = GasGraph(cityGates=gas_points, vizinhos=vizinhos)
 
-entradas = gas_points.loc[gas_points["type"] == "entry"]
+# entradas = gas_points[(gas_points[volumeProperty] >= 0) & (gas_points[typeProperty] != 'exit')]
+entradas = gas_points[(gas_points[typeProperty] == 'entry')]
+
 caminhos = grafo.ObtemCaminhos(entradas['id'])
+
+ids = entradas['id'].unique()
+
+# Regex que pega o id no dentro do nome gerado no caminho <name>(<id>)
+escaped_ids = [re.escape(str(id_)) for id_ in ids]
+pattern = r'.*\((' + '|'.join(escaped_ids) + r')\)'
+
+# Seleciona as colunas de entrada a serem removidas
+cols_to_remove = caminhos.filter(regex=pattern).columns
+# Remove as entradas dos caminhos
+caminhos = caminhos.drop(columns=cols_to_remove)
 caminhos.to_csv(
-    path_or_buf="distancias.csv", index=False, decimal=",", encoding="Windows-1252"
+    path_or_buf="distancias_tgn.csv", index=False, decimal=",", encoding="Windows-1252"
 )
